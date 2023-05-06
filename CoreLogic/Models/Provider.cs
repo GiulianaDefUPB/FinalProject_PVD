91c9f4c76aa0a8ea3ae2218401ce5fe734e918d6
GiulianaDefUPB <giulianadefilippisv1@upb.edu>
2023-05-06 13:39:49 -0400

    Develop Created CoreLogic class library and Models folder. Added Provider.cs in Models.

---
+++ CoreLogic/Models/Provider.cs
@@ -0,0 +1,15 @@
+
+namespace UPB.CoreLogic.Models;
+
+public class Provider
+{
+    private string Name {get; set;}
+    private string Address {get; set;}
+    private string Category {get; set;}
+    private int PhoneNumber {get; set;}
+    private int ContractRemainingDays {get; set;}
+    private  DateTime ContractExpirationDate {get; set;}
+    private bool ExpiredContract {get; set;}
+    private bool Enable {get; set;}
+
+}
