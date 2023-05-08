35ec86adfc9afcf2b65044189dc78499c86866e7
Laura Delgadillo <delgadilloarterolaura@gmail.com>
2023-05-08 14:46:01 -0400

    PVD-001 Added the ReadProviderFromFile method to read a provider from a file by its ID and return the provider object if found, or null if not found in ProviderManager.cs. Also, updated the Create method to validate input values and verify if a provider with the same ID already exists. Finally, added ‘providers.txt’ in gitignore to exclude providers files.

--- CoreLogic/Managers/ProviderManager.cs
+++ CoreLogic/Managers/ProviderManager.cs
@@ -14,6 +14,21 @@ public class ProviderManager
 
     public Provider Create(Provider providerToCreate)
     {
+         if (providerToCreate.ID < 0 
+            || string.IsNullOrEmpty(providerToCreate.Name)
+            || string.IsNullOrEmpty(providerToCreate.Address)
+            || string.IsNullOrEmpty(providerToCreate.Category)
+            || providerToCreate.PhoneNumber <= 0
+            || providerToCreate.ContractExpirationDate == DateTime.MinValue)
+        {
+            throw new Exception("Invalid input parameters.");
+        }
+
+        if (ReadProviderFromFile(providerToCreate.ID) != null)
+        {
+            throw new Exception("Provider with the same ID already exists");
+        }
+
         int remainingDays = (int)providerToCreate.ContractExpirationDate.Subtract(DateTime.Today).TotalDays;
         bool isExpired = remainingDays < 0;
 
@@ -33,6 +48,46 @@ public class ProviderManager
         WriteProviderToFile(createdProvider);
 
         return createdProvider;
+    }
+
+    public Provider? ReadProviderFromFile(int id)
+    {
+        if (!File.Exists(_filePath))
+        {
+            return null;
+        }
+
+        StreamReader reader = new StreamReader(_filePath);
+
+        string? line = reader.ReadLine();
+        while (line != null)
+        {
+            string[] providerInfo = line.Split(',');
+            int providerId = int.Parse(providerInfo[0]);
+
+            if (providerId == id)
+            {
+                reader.Close();
+                Provider foundProvider = new Provider()
+                {
+                    ID = providerId,
+                    Name = providerInfo[1],
+                    Address = providerInfo[2],
+                    Category = providerInfo[3],
+                    PhoneNumber = int.Parse(providerInfo[4]),
+                    ContractRemainingDays = int.Parse(providerInfo[5]),
+                    ContractExpirationDate = DateTime.Parse(providerInfo[6]),
+                    ExpiredContract = Boolean.Parse(providerInfo[7]),
+                    Enable = Boolean.Parse(providerInfo[8])
+                };
+                return foundProvider;
+            }
+
+            line = reader.ReadLine();
+        }
+
+        reader.Close();
+        return null;
     }
 
     public void WriteProviderToFile(Provider provider)
