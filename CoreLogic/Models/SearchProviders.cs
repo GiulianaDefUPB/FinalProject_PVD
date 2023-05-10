b68f521cd955e82023f64bc2b9cc9e5b852a5af2
Mateo Mercado <matemerc@gmail.com>
2023-05-09 23:00:43 -0400

    PVD-007 SearchProviders file added (backing service). Atributes: name, address and phone_number

---
+++ CoreLogic/Models/SearchProviders.cs
@@ -0,0 +1,8 @@
+namespace UPB.CoreLogic.Models;
+
+public class SearchProviders
+{
+    public string name { get; set; }
+    public Address address { get; set; }
+    public string phone_number { get; set; }
+}
