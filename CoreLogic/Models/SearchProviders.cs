b6eed892333aaec2576fdca1ae43e51d5c84b432
Mateo Mercado <matemerc@gmail.com>
2023-05-09 23:03:08 -0400

    PVD-007 SearchProviders file edited. Address atribute fixed.

--- CoreLogic/Models/SearchProviders.cs
+++ CoreLogic/Models/SearchProviders.cs
@@ -3,6 +3,6 @@ namespace UPB.CoreLogic.Models;
 public class SearchProviders
 {
     public string name { get; set; }
-    public Address address { get; set; }
+    public string address { get; set; }
     public string phone_number { get; set; }
 }
